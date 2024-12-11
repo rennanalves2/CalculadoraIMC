a8b2f5a27e0f0c4661301a9d22243ec51f34a432
Rennan Alves <rennan_areas@hotmail.com>
2024-12-10 22:53:59 -0300

    Implements css

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ function CalcularIMC(event) {
     var resultado;
     if(imc < 17) {
         resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Cuidado você está muito abaixo do peso";
+        resultado.innerHTML = "<br/><h1> Seu resultado foi: " + imc.toFixed(2) + "<br/> Cuidado você está muito abaixo do peso</h1>";
     }
     else if(imc > 17 && imc <= 18.49) {
         resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está abaixo do peso";
+        resultado.innerHTML = "<br/><h1> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está abaixo do peso</h1>";
     }
     else if(imc > 18.5 && imc <= 24.99) {
         resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está no peso ideal";
+        resultado.innerHTML = "<br/><h1> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está no peso ideal</h1>";
     }
     else if(imc > 25 && imc <= 29.99) {
         resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está acima do peso";
+        resultado.innerHTML = "<br/><h1> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está acima do peso</h1>";
     }
     else if(imc >= 30) {
         resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Cuidado você está na obesidade";
+        resultado.innerHTML = "<br/><h1> Seu resultado foi: " + imc.toFixed(2) + "<br/> Cuidado você está na obesidade</h1>";
     }
 }
