ff6219611d0ec81ac500a4473d95e060dda8b24d
Rennan Alves <rennan_areas@hotmail.com>
2024-12-10 22:34:31 -0300

    implements script

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert("Rennan");
+function CalcularIMC(event) {
+    event.preventDefault();
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+    let imc = peso / (altura * altura);
+    var resultado;
+    if(imc < 17) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Cuidado você está muito abaixo do peso";
+    }
+    else if(imc > 17 && imc <= 18.49) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está abaixo do peso";
+    }
+    else if(imc > 18.5 && imc <= 24.99) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está no peso ideal";
+    }
+    else if(imc > 25 && imc <= 29.99) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está acima do peso";
+    }
+    else if(imc >= 30) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Cuidado você está na obesidade";
+    }
+}
